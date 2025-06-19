07b224a0bc0a7c28dadb3aec98b507539b350dc9
FamSP <tame2543@gmail.com>
2025-06-19 12:18:24 +0700

    first commit

---
+++ src/Componants/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Card = (props) => {
+  return (
+    <div className="card bg-base-100 w-96 shadow-sm">
+      <figure>
+        <img src={props.img} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{props.title}</h2>
+        <p>{props.type}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+    
+  );
+};
+
+export default Card;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Navbar from "../Componants/Navbar";
+import Restaurants from "../Componants/Restaurants";
+
+const Home = () => {
+  return (
+    <div className="container mx-auto">
+      <Navbar />
+      <h1 className="title justify-center text-3xl text-center m-5 p-5">
+        {" "}
+        Grab restaurant
+      </h1>
+      <div className="mb-5 flex justify-center items-center">
+        <label className="input flex items-center gap-2 w-2xl">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input type="search" required placeholder="Search" />
+        </label>
+      </div>
+      //result
+      <Restaurants />
+    </div>
+  );
+};
+
+export default Home;
